1c38ecfe2c2e8a91e3558396e4045f611206d1af
Philip Ross <rosspa@rose-hulman.edu>
2016-04-24 23:38:53 -0400

    start

---
+++ src/editortrees/EditTree.java
@@ -0,0 +1,341 @@
+package editortrees;
+
+import debughelp.DisplayableBinaryTree;
+
+// A height-balanced binary tree with rank that could be the basis for a text editor.
+
+public class EditTree {
+
+	public Node root;
+	public int rotCount = 0;
+	final static Node NULL_NODE = new Node();
+	public boolean rootChanged;
+	public DisplayableBinaryTree display;
+
+	/**
+	 * Construct an empty tree
+	 */
+	public EditTree() {
+		this.root = NULL_NODE;
+
+	}
+
+	/**
+	 * Construct a single-node tree whose element is c
+	 * 
+	 * @param c
+	 */
+	public EditTree(char c) {
+		this.root = new Node(c);
+	}
+
+	/**
+	 * Create an EditTree whose toString is s. This can be done in O(N) time,
+	 * where N is the length of the tree (repeatedly calling insert() would be
+	 * O(N log N), so you need to find a more efficient way to do this.
+	 * 
+	 * @param s
+	 */
+	public EditTree(String s) {
+
+	}
+
+	/**
+	 * Make this tree be a copy of e, with all new nodes, but the same shape and
+	 * contents.
+	 * 
+	 * @param e
+	 */
+	public EditTree(EditTree e) {
+		this.root = NULL_NODE;
+		this.root = this.root.copy(e.root);
+	}
+
+	/**
+	 * 
+	 * @return the height of this tree
+	 */
+	public int height() {
+		return this.root.height();
+	}
+
+	/**
+	 * 
+	 * returns the total number of rotations done in this tree since it was
+	 * created. A double rotation counts as two.
+	 *
+	 * @return number of rotations since tree was created.
+	 */
+	public int totalRotationCount() {
+		return this.rotCount; // replace by a real calculation.
+	}
+
+	/**
+	 * return the string produced by an inorder traversal of this tree
+	 */
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		this.root.stringify(sb);
+		return sb.toString(); // replace by a real calculation.
+
+	}
+
+	/**
+	 * This one asks for more info from each node. You can write it like the
+	 * arraylist-based toString() method from the BST assignment. However, the
+	 * output isn't just the elements, but the elements, ranks, and balance
+	 * codes. Former CSSE230 students recommended that this method, while making
+	 * it harder to pass tests initially, saves them time later since it catches
+	 * weird errors that occur when you don't update ranks and balance codes
+	 * correctly. For the tree with node b and children a and c, it should
+	 * return the string: [b1=, a0=, c0=] There are many more examples in the
+	 * unit tests.
+	 * 
+	 * @return The string of elements, ranks, and balance codes, given in a
+	 *         pre-order traversal of the tree.
+	 */
+	public String toDebugString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		this.root.stringifyDebug(sb);
+		if (sb.length() > 2) {
+			sb.delete(sb.length() - 2, sb.length());
+		}
+		return sb.append("]").toString();
+	}
+
+	/**
+	 * 
+	 * @param pos
+	 *            position in the tree
+	 * @return the character at that position
+	 * @throws IndexOutOfBoundsException
+	 */
+	public char get(int pos) throws IndexOutOfBoundsException {
+		if (pos < 0 || pos > this.root.size()) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		Node current = this.root.get(pos);
+		return current.element;
+	}
+
+	/**
+	 * 
+	 * @param c
+	 *            character to add to the end of this tree.
+	 */
+	public void add(char c) {
+		// Notes:
+		// 1. Please document chunks of code as you go. Why are you doing what
+		// you are doing? Comments written after the code is finalized tend to
+		// be useless, since they just say WHAT the code does, line by line,
+		// rather than WHY the code was written like that. Six months from now,
+		// it's the reasoning behind doing what you did that will be valuable to
+		// you!
+		// 2. Unit tests are cumulative, and many things are based on add(), so
+		// make sure that you get this one correct.
+		Node newN = this.root.add(c, NULL_NODE, this);
+		if (!this.rootChanged) {
+			this.root = newN;
+		}
+		this.rootChanged = false;
+
+	}
+
+	public void setRoot(Node n) {
+		this.root = n;
+		this.rootChanged = true;
+	}
+
+	/**
+	 * 
+	 * @param c
+	 *            character to add
+	 * @param pos
+	 *            character added in this inorder position
+	 * @throws IndexOutOfBoundsException
+	 *             id pos is negative or too large for this tree
+	 */
+	public void add(char c, int pos) throws IndexOutOfBoundsException {
+		// might be able to do this better by placing it in Node class
+		if (pos < 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (this.root == EditTree.NULL_NODE) {
+			if (pos != 0) {
+				throw new IndexOutOfBoundsException();
+			}
+			this.root = new Node(c);
+			this.root.parent = EditTree.NULL_NODE;
+		} else {
+			this.root.add(c, NULL_NODE, this, pos);
+
+		}
+	}
+
+	/**
+	 * 
+	 * @return the number of nodes in this tree
+	 */
+	public int size() {
+		return this.root.size(); // replace by a real calculation.
+	}
+
+	/**
+	 * 
+	 * @param pos
+	 *            position of character to delete from this tree
+	 * @return the character that is deleted
+	 * @throws IndexOutOfBoundsException
+	 */
+	public char delete(int pos) throws IndexOutOfBoundsException {
+		// Implementation requirement:
+		// When deleting a node with two children, you normally replace the
+		// node to be deleted with either its in-order successor or predecessor.
+		// The tests assume assume that you will replace it with the
+		// *successor*.
+		if (this.root != NULL_NODE && pos >= 0) {
+			// will check to see if pos > than number of nodes in tree inside
+			// the node class and throw an exception there
+			return this.root.delete(pos, this);
+		}
+		throw new IndexOutOfBoundsException();
+	}
+
+	/**
+	 * This method operates in O(length*log N), where N is the size of this
+	 * tree.
+	 * 
+	 * @param pos
+	 *            location of the beginning of the string to retrieve
+	 * @param length
+	 *            length of the string to retrieve
+	 * @return string of length that starts in position pos
+	 * @throws IndexOutOfBoundsException
+	 *             unless both pos and pos+length-1 are legitimate indexes
+	 *             within this tree.
+	 */
+	public String get(int pos, int length) throws IndexOutOfBoundsException {
+		return "";
+	}
+
+	/**
+	 * This method is provided for you, and should not need to be changed. If
+	 * split() and concatenate() are O(log N) operations as required, delete
+	 * should also be O(log N)
+	 * 
+	 * @param start
+	 *            position of beginning of string to delete
+	 * 
+	 * @param length
+	 *            length of string to delete
+	 * @return an EditTree containing the deleted string
+	 * @throws IndexOutOfBoundsException
+	 *             unless both start and start+length-1 are in range for this
+	 *             tree.
+	 */
+	public EditTree delete(int start, int length) throws IndexOutOfBoundsException {
+		if (start < 0 || start + length >= this.size())
+			throw new IndexOutOfBoundsException(
+					(start < 0) ? "negative first argument to delete" : "delete range extends past end of string");
+		EditTree t2 = this.split(start);
+		EditTree t3 = t2.split(length);
+		this.concatenate(t3);
+		return t2;
+	}
+
+	/**
+	 * Append (in time proportional to the log of the size of the larger tree)
+	 * the contents of the other tree to this one. Other should be made empty
+	 * after this operation.
+	 * 
+	 * @param other
+	 * @throws IllegalArgumentException
+	 *             if this == other
+	 */
+	public void concatenate(EditTree other) throws IllegalArgumentException {
+
+	}
+
+	/**
+	 * This operation must be done in time proportional to the height of this
+	 * tree.
+	 * 
+	 * @param pos
+	 *            where to split this tree
+	 * @return a new tree containing all of the elements of this tree whose
+	 *         positions are >= position. Their nodes are removed from this
+	 *         tree.
+	 * @throws IndexOutOfBoundsException
+	 */
+	public EditTree split(int pos) throws IndexOutOfBoundsException {
+		return null; // replace by a real calculation.
+	}
+
+	/**
+	 * Don't worry if you can't do this one efficiently.
+	 * 
+	 * @param s
+	 *            the string to look for
+	 * @return the position in this tree of the first occurrence of s; -1 if s
+	 *         does not occur
+	 */
+	public int find(String s) {
+		return -2;
+	}
+
+	/**
+	 * 
+	 * @param s
+	 *            the string to search for
+	 * @param pos
+	 *            the position in the tree to begin the search
+	 * @return the position in this tree of the first occurrence of s that does
+	 *         not occur before position pos; -1 if s does not occur
+	 */
+	public int find(String s, int pos) {
+		return -2;
+	}
+
+	/**
+	 * @return The root of this tree.
+	 */
+	public Node getRoot() {
+		if (this.root == NULL_NODE) {
+			return null;
+		}
+		return this.root;
+	}
+
+	public int slowHeight() {
+		if (this.root == NULL_NODE) {
+			return -1;
+		}
+		return this.root.slowHeight();
+	}
+
+	public int slowSize() {
+		return this.root.size();
+	}
+
+	public void show() {
+		if (this.display == null) {
+			this.display = new DisplayableBinaryTree(this, 960, 1080, true);
+		} else {
+			this.display.show(true);
+		}
+
+	}
+
+	/**
+	 * closes the tree window still keeps all the data and you can still reshow
+	 * the tree with the show method
+	 */
+	public void close() {
+		if (this.display != null) {
+			this.display.close();
+		}
+	}
+}
---
+++ src/editortrees/Node.java
@@ -0,0 +1,485 @@
+package editortrees;
+
+import java.util.function.IntSupplier;
+
+import debughelp.DisplayableNodeWrapper;
+
+public class Node {
+
+	enum Code {
+		SAME, LEFT, RIGHT;
+		// Used in the displayer and debug string
+		@Override
+		public String toString() {
+			switch (this) {
+			case LEFT:
+				return "/";
+			case SAME:
+				return "=";
+			case RIGHT:
+				return "\\";
+			default:
+				throw new IllegalStateException();
+			}
+		}
+	}
+
+	// The fields would normally be private, but for the purposes of this class,
+	// we want to be able to test the results of the algorithms in addition to
+	// the
+	// "publicly visible" effects
+
+	char element;
+	Node left, right, parent; // subtrees and parent
+	int rank; // inorder position of this node within its own subtree.
+	Code balance;
+	DisplayableNodeWrapper wrapper;
+
+	public Node() {
+		this.balance = Code.SAME;
+	}
+
+	public Node(char element) {
+		this.element = element;
+		this.balance = Code.SAME;
+		this.left = EditTree.NULL_NODE;
+		this.right = EditTree.NULL_NODE;
+		this.parent = EditTree.NULL_NODE;
+		this.wrapper = new DisplayableNodeWrapper(this);
+	}
+
+	class Rank implements IntSupplier {
+
+		@Override
+		public int getAsInt() {
+			// TODO Auto-generated method stub.
+			return Node.this.left.size();
+		}
+
+	}
+
+	// For the following methods, you should fill in the details so that they
+	// work correctly
+	public int height() {
+		if (this == EditTree.NULL_NODE) {
+			return -1;
+		}else if (this.balance == Code.LEFT){
+			return 1+ this.left.height();
+		}else if (this.balance == Code.RIGHT){
+			return 1 + this.right.height();
+		}else{
+			return 1+ this.right.height();
+		}
+		//return Math.max(1 + this.left.height(), 1 + this.right.height());
+	}
+
+	/**
+	 * Recursively finds the "size" of the tree, ie. the number of nodes in the
+	 * tree
+	 *
+	 * @return size of tree starting from "this"
+	 */
+	public int size() {
+		if (this == EditTree.NULL_NODE) {
+			return 0;
+		}
+		return 1 + this.left.size() + this.right.size();
+	}
+
+	/**
+	 * Adds a new node to the tree at the far right
+	 *
+	 * @param c
+	 *            element for new Node
+	 * @param parent1
+	 *            parent of the new Node
+	 * @param tree
+	 *            editor tree from which this method was initially called from;
+	 *            used in other function calls
+	 * @return the new Node created
+	 */
+	public Node add(char c, Node parent1, EditTree tree) {
+		//
+		if (this == EditTree.NULL_NODE) {
+			Node n = new Node(c);
+			n.parent = parent1;
+			parent1.right = n;
+			n.rank = 0;
+			n.checkRotate(false, tree);
+
+			return n;
+		} else if (this.right == EditTree.NULL_NODE) {
+			this.right = this.right.add(c, this, tree);
+
+		} else {
+			this.right.add(c, this, tree);
+		}
+		return this;
+
+	}
+
+	public Node get(int pos) throws IndexOutOfBoundsException {
+		if (this == EditTree.NULL_NODE)
+			throw new IndexOutOfBoundsException();
+		if (this.rank == pos)
+			return this;
+		else if (this.rank > pos) {
+			return this.left.get(pos);
+		} else {
+			pos = pos - this.rank - 1;
+			return this.right.get(pos);
+		}
+	}
+
+	public void stringify(StringBuilder sb) {
+		if (this == EditTree.NULL_NODE) {
+			return;
+		}
+
+		this.left.stringify(sb);
+		sb.append(this.element);
+		this.right.stringify(sb);
+	}
+
+	public void stringifyDebug(StringBuilder sb) {
+		if (this == EditTree.NULL_NODE) {
+			return;
+		}
+		sb.append(this.element);
+		sb.append(this.rank);
+		sb.append(this.balance);
+		sb.append(", ");
+		this.left.stringifyDebug(sb);
+		this.right.stringifyDebug(sb);
+
+	}
+
+	public void checkRotate(boolean isLeft, EditTree tree) {
+		Node p = this.parent;
+		Node c = this;
+		if (c == EditTree.NULL_NODE) {
+			return;
+		}
+		while (p != EditTree.NULL_NODE) {
+
+			if (p.getBalance() == Code.SAME && !isLeft) {
+				p.setBalace(Code.RIGHT);
+			} else if (p.getBalance() == Code.SAME && isLeft) {
+				p.setBalace(Code.LEFT);
+			} else if ((p.getBalance() == Code.LEFT && !isLeft) || (p.getBalance() == Code.RIGHT && isLeft)) {
+				p.setBalace(Code.SAME);
+				break;
+			} else {
+				p = p.doRotation(c, tree);
+				break;
+			}
+			c = p;
+			p = p.parent;
+			if (p.left == c) {
+				isLeft = true;
+			} else {
+				isLeft = false;
+			}
+		}
+
+	}
+
+	public Node singleRR(Node b, EditTree tree) {
+		tree.rotCount++;
+		Node a = this;
+		Node apar = a.parent;
+		Node bright = b.right;
+
+		if (a.parent == EditTree.NULL_NODE) {
+			b.parent = EditTree.NULL_NODE;
+			tree.setRoot(b);
+		} else {
+			b.parent = apar;
+
+			if (a == apar.left) {
+				apar.left = b;
+			} else {
+				apar.right = b;
+			}
+		}
+
+		a.left = bright;
+		a.parent = b;
+		b.right = a;
+		bright.parent = a;
+
+		a.rank = a.rank - (b.rank + 1);
+
+		int lh = a.left.height();
+		int rh = a.right.height();
+		b.setBalace(Code.SAME);
+		if (lh > rh)
+			a.setBalace(Code.LEFT);
+		else if (lh == rh)
+			a.setBalace(Code.SAME);
+		else
+			a.setBalace(Code.RIGHT);
+
+		return b;
+	}
+
+	public Node singleLR(Node b, EditTree tree) {
+		tree.rotCount++;
+		Node a = this;
+		Node Apar = a.parent;
+		Node bleft = b.left;
+
+		if (a.parent == EditTree.NULL_NODE) {
+			b.parent = EditTree.NULL_NODE;
+			tree.setRoot(b);
+		} else {
+			b.parent = Apar;
+			if (a == a.parent.left) {
+				Apar.left = b;
+			} else {
+				Apar.right = b;
+			}
+		}
+
+		a.right = bleft;
+		a.parent = b;
+		b.left = a;
+		bleft.parent = a;
+
+		b.rank = a.rank + b.rank + 1;
+
+		int lh = a.left.height();
+		int rh = a.right.height();
+
+		b.setBalace(Code.SAME);
+
+		if (lh > rh)
+			a.setBalace(Code.LEFT);
+		else if (lh == rh)
+			a.setBalace(Code.SAME);
+		else
+			a.setBalace(Code.RIGHT);
+
+		return b;
+	}
+
+	public Node doRotation(Node b, EditTree tree) {
+		// No need to check if caller is a nullnode, checkRotate screens out
+		// that case
+		// Single right rotation
+		if (this.getBalance() == Code.LEFT && b.getBalance() == Code.LEFT) {
+			return this.singleRR(b, tree);
+		} // Double right rotation
+		else if (this.getBalance() == Code.LEFT && b.getBalance() == Code.RIGHT) {
+
+			Node newB = b.singleLR(b.right, tree);
+			Node output = this.singleRR(newB, tree);
+
+			return output;
+		} // Double left rotation
+		else if (this.getBalance() == Code.RIGHT && b.getBalance() == Code.LEFT) {
+			Node newB = b.singleRR(b.left, tree);
+			Node output = this.singleLR(newB, tree);
+
+			return output;
+		} // Single left rotation
+		else {
+			return this.singleLR(b, tree);
+		}
+	}
+
+	private void setBalace(Code newBalance) {
+		this.balance = newBalance;
+	}
+
+	public Code getBalance() {
+		return this.balance;
+	}
+
+	public void add(char c, Node parent1, EditTree tree, int pos) {
+		if (this.left == EditTree.NULL_NODE && this.right == EditTree.NULL_NODE) {
+			if (pos > 1) {
+				throw new IndexOutOfBoundsException();
+			}
+		}
+
+		if (pos <= this.rank) {
+			if (this.left == EditTree.NULL_NODE) {
+				this.left = new Node(c);
+				this.left.parent = this;
+				this.rank++;
+				this.left.checkRotate(true, tree);
+
+			} else {
+				this.rank++;
+				this.left.add(c, this, tree, pos);
+
+			}
+
+		} else if (pos > this.rank) {
+			if (this.right == EditTree.NULL_NODE) {
+				this.right = new Node(c);
+				this.right.parent = this;
+				this.right.checkRotate(false, tree);
+			} else {
+				this.right.add(c, this, tree, pos - (this.rank + 1));
+			}
+		}
+
+	}
+
+
+
+	public Node copy(Node oldNode) {
+		if (oldNode == EditTree.NULL_NODE) {
+			return EditTree.NULL_NODE;
+		}
+		Node toReturn = new Node(oldNode.element);
+		toReturn.rank = oldNode.rank;
+		toReturn.balance = oldNode.balance;
+		toReturn.left = toReturn.copy(oldNode.left);
+		toReturn.left.parent = toReturn;
+		toReturn.right = toReturn.copy(oldNode.right);
+		toReturn.right.parent = toReturn;
+		return toReturn;
+	}
+
+	public char delete(int pos, EditTree tree) {
+		
+		if (this == EditTree.NULL_NODE) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (this.left == EditTree.NULL_NODE && this.right == EditTree.NULL_NODE) {
+			if (pos > 1) {
+				throw new IndexOutOfBoundsException();
+			}
+		}
+		if (pos > this.rank) {
+			return this.right.delete(pos - (this.rank + 1), tree);
+		} else if (pos < this.rank) {
+			this.rank--;
+			return this.left.delete(pos, tree);
+		} else {
+			if (this.parent.left == this) {
+				if (this.left == EditTree.NULL_NODE && this.right == EditTree.NULL_NODE) {
+					this.parent.left = EditTree.NULL_NODE;
+					this.parent.right.checkRotate(false, tree);
+
+				} else if (this.left == EditTree.NULL_NODE && this.right != EditTree.NULL_NODE) {
+					this.parent.left = this.right;
+					this.right.parent = this.parent.left;
+					this.parent.left.checkRotate(true, tree);
+				} else if (this.left != EditTree.NULL_NODE && this.right == EditTree.NULL_NODE) {
+					this.parent.left = this.left;
+					this.left.parent = this.parent.left;
+					this.parent.left.checkRotate(true, tree);
+				}else{
+				Node current = this.right;
+				if (current.left != EditTree.NULL_NODE) {
+					while (current.left != EditTree.NULL_NODE) {
+						current = current.left;
+					}
+				}
+				char toReturn = this.element;
+				this.element = current.element;
+				current.element = toReturn;
+				return this.delete(this.rank + 1, tree);
+				}
+			} else {
+				if (this.left == EditTree.NULL_NODE && this.right == EditTree.NULL_NODE) {
+					this.parent.right = EditTree.NULL_NODE;
+					this.parent.left.checkRotate(true, tree);
+
+				} else if (this.left == EditTree.NULL_NODE && this.right != EditTree.NULL_NODE) {
+					this.parent.right = this.right;
+					this.right.parent = this.parent.right;
+					this.parent.right.checkRotate(false, tree);
+				} else if (this.left != EditTree.NULL_NODE && this.right == EditTree.NULL_NODE) {
+					this.parent.right = this.left;
+					this.left.parent = this.parent.right;
+					this.parent.right.checkRotate(false, tree);
+				}
+				else{
+					Node current = this.right;
+				if (current.left != EditTree.NULL_NODE){
+					while (current.left != EditTree.NULL_NODE) {
+						current = current.left;
+					}
+				}
+				char toReturn = this.element;
+				this.element = current.element;
+				current.element = toReturn;
+				return this.delete( this.rank + 1, tree);
+			}
+			}
+			
+		}
+
+		return this.element;
+		// TODO Auto-generated method stub.
+		// If no children, make this a null node
+
+		// If one child, make this data child's data and delete child. Check
+		// rotation
+
+		// If two children, make this data this.right.data (or
+		// this.parent.right.data, etc) and remove other node. Check rotation
+
+	}
+	public int slowHeight() {
+		// base case
+		if (this == EditTree.NULL_NODE) {
+			return 0;
+		}
+		return 1 + Math.max(this.left.height(), this.right.height());
+	}
+
+	public int slowSize() {
+		if (this == EditTree.NULL_NODE) {
+			return 0;
+		}
+		return this.left.size() + this.right.size() + 1;
+	}
+
+	public DisplayableNodeWrapper getDisplayableNodePart() {
+		return this.wrapper;
+	}
+
+	public boolean hasLeft() {
+		return this.left != EditTree.NULL_NODE;
+	}
+
+	public Node getLeft() {
+		if (this.left == EditTree.NULL_NODE) {
+			return null;
+		}
+		return this.left;
+	}
+
+	public boolean hasRight() {
+		return this.right != EditTree.NULL_NODE;
+	}
+
+	public Node getRight() {
+		if (this.right == EditTree.NULL_NODE) {
+			return null;
+		}
+		return this.right;
+	}
+
+	public boolean hasParent() {
+		return this.parent != EditTree.NULL_NODE;
+	}
+
+	public Node getParent() {
+		return this.parent;
+	}
+
+	public int getRank() {
+		return this.rank;
+	}
+
+	public char getElement() {
+		return this.element;
+	}
+}
